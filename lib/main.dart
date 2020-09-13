08ff1952c612cd44e9e92a5d86e0781a9c6ccb6c
Haaragard <diogololz159@gmail.com>
2020-09-13 17:45:24 -0300

    Adicionado funções de calcular e mostrar valores, junto de função de reset

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,42 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final TextEditingController _weightController = TextEditingController();
+  final TextEditingController _heightController = TextEditingController();
+
+  String _textInfo = "Informe seus dados";
+
+  void _resetFields() {
+    _weightController.text = "";
+    _heightController.text = "";
+
+    setState(() {
+      _textInfo = "Informe seus dados";
+    });
+  }
+
+  void _calculate() {
+    double weight = double.parse(_weightController.text);
+    double height = (double.parse(_heightController.text) / 100);
+    double imc = weight / (height * height);
+
+    setState(() {
+      if (imc < 18.6) {
+        _textInfo = "Abaixo do peso (${imc.toStringAsPrecision(2)})";
+      } else if ((imc >= 18.6) && (imc < 24.9)) {
+        _textInfo = "Peso ideal (${imc.toStringAsPrecision(2)})";
+      } else if ((imc >= 24.9) && (imc < 29.9)) {
+        _textInfo = "Levemente acima do peso (${imc.toStringAsPrecision(2)})";
+      } else if ((imc >= 29.9) && (imc < 34.9)) {
+        _textInfo = "Obesidade grau I (${imc.toStringAsPrecision(2)})";
+      } else if ((imc >= 34.9) && (imc < 39.9)) {
+        _textInfo = "Obesidade grau II (${imc.toStringAsPrecision(2)})";
+      } else if (imc >= 39.9) {
+        _textInfo = "Obesidade grau III (${imc.toStringAsPrecision(2)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +58,7 @@ class _HomeState extends State<Home> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.refresh),
-            onPressed: () {},
+            onPressed: _resetFields,
           ),
         ],
       ),
@@ -45,6 +81,7 @@ class _HomeState extends State<Home> {
             ),
             textAlign: TextAlign.center,
             style: TextStyle(color: Colors.green, fontSize: 25.0),
+            controller: _weightController,
           ),
 
           // Altura
@@ -54,6 +91,7 @@ class _HomeState extends State<Home> {
             ),
             textAlign: TextAlign.center,
             style: TextStyle(color: Colors.green, fontSize: 25.0),
+            controller: _heightController,
           ),
 
           Padding( padding: EdgeInsets.only(top: 15.0),),
@@ -62,7 +100,7 @@ class _HomeState extends State<Home> {
           Container(
             height: 50.0,
             child: RaisedButton(
-              onPressed: () {},
+              onPressed: _calculate,
               child: Text("Calcular",
                 style: TextStyle(color: Colors.white, fontSize: 20.0),
               ),
@@ -73,7 +111,7 @@ class _HomeState extends State<Home> {
           Padding( padding: EdgeInsets.only(top: 15.0),),
 
           // Infromação
-          Text("Info",
+          Text("$_textInfo",
             textAlign: TextAlign.center,
             style: TextStyle(color: Colors.green, fontSize: 25.0),
           ),
