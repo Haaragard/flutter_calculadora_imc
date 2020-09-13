344b4ba6a0b9802512b32933abda2e4bf5c306c0
Haaragard <diogololz159@gmail.com>
2020-09-13 17:20:05 -0300

    Adicionado botão e texto de info, também melhorado o layout

--- lib/main.dart
+++ lib/main.dart
@@ -32,27 +32,52 @@ class _HomeState extends State<Home> {
   }
 
   Widget _body() {
-    return Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+    return
+      ListView(
+        padding: EdgeInsets.only(left: 15, right: 15),
         children: <Widget>[
           Icon(Icons.person_outline, size: 120, color: Colors.green,),
 
           // Peso
           TextField(keyboardType: TextInputType.number,
-            decoration: InputDecoration(labelText: "Peso (kg)", labelStyle: TextStyle(color: Colors.green)),
+            decoration: InputDecoration(labelText: "Peso (kg)",
+                labelStyle: TextStyle(color: Colors.green)
+            ),
             textAlign: TextAlign.center,
             style: TextStyle(color: Colors.green, fontSize: 25.0),
           ),
 
           // Altura
           TextField(keyboardType: TextInputType.number,
-            decoration: InputDecoration(labelText: "Altura (cm)", labelStyle: TextStyle(color: Colors.green)),
+            decoration: InputDecoration(labelText: "Altura (cm)",
+                labelStyle: TextStyle(color: Colors.green)
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          ),
+
+          Padding( padding: EdgeInsets.only(top: 15.0),),
+
+          // Botão para calcular
+          Container(
+            height: 50.0,
+            child: RaisedButton(
+              onPressed: () {},
+              child: Text("Calcular",
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+              color: Colors.green,
+            ),
+          ),
+
+          Padding( padding: EdgeInsets.only(top: 15.0),),
+
+          // Infromação
+          Text("Info",
             textAlign: TextAlign.center,
             style: TextStyle(color: Colors.green, fontSize: 25.0),
           ),
         ],
-      ),
-    );
+      );
   }
 }
